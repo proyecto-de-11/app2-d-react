fcdc2989f36ec6bf008a0effc78b977937be8a8d
cs25001 <cs25001@esfe.agape.edu.sv>
2025-11-20 16:30:25 +0000

    ESCONDIENDO BARRA

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,12 @@
 
 import { Stack } from 'expo-router';
-import { Colors } from '@/constants/theme';
 
 export default function RootLayout() {
   return (
     <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Colors.light.tint,
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
+    
     >
-      <Stack.Screen name="index" options={{ title: 'Bienvenido' }} />
+      <Stack.Screen name="index" options={{headerShown:false}}  />
       <Stack.Screen name="screens/LoginScreen" options={{ title: 'Iniciar Sesión' }} />
       <Stack.Screen name="screens/HomeScreen" options={{ title: 'Inicio' }} />
     </Stack>
